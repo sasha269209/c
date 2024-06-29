2bfce25eaf79afef6daa412a752ef0e21ccec35f
sasha269209 <work269209@gmail.com>
2024-06-30 00:13:28 +0200

    add new file

--- 001_c.c
+++ 001_c.c
@@ -1,1 +1,13 @@
-hhsdsds
+//Lessons 1 (FromtheLAMERtotheProgrammer - 46 lessons)
+
+#include <stdio.h>
+
+int main ()
+{
+	printf("My first program in C language\n");
+	printf("Helo World\n");
+	//My first program
+	/*
+	My next - big comment
+	*/
+}
